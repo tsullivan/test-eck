b250a09abdd3399620e37e5c7c799c042e0f1610
Timothy Sullivan <tsullivan@elastic.co>
2022-04-27 11:18:15 -0700

    --wip-- [skip ci]

--- install-eck.sh
+++ install-eck.sh
@@ -5,7 +5,7 @@ set -v -e
 # install custom resource definitions
 kubectl create -f https://download.elastic.co/downloads/eck/2.1.0/crds.yaml
 
-# install the operator with rbac rules
+# install the operator named `elastic-system` with rbac rules
 kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/operator.yaml
 
 # to monitor the operator logs
