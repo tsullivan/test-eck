c07239722819489e53685dc03f3ef67e1b4fb6ec
Timothy Sullivan <tsullivan@elastic.co>
2022-04-27 11:04:48 -0700

    trial license script

---
+++ start-trial-license.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -v -e
+
+# To start a trial create a Kubernetes secret. Note that it must be in the same namespace as the operator
+# (elastic-system)
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: eck-trial-license
+  namespace: elastic-system
+  labels:
+    license.k8s.elastic.co/type: enterprise_trial
+  annotations:
+    elastic.co/eula: accepted
+EOF
