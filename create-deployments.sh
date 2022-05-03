5bbe8888f7146e8da25242c70d0c81316de63dac
Timothy Sullivan <tsullivan@elastic.co>
2022-05-03 12:42:10 -0700

    script that installs license

--- create-deployments.sh
+++ create-deployments.sh
@@ -5,3 +5,17 @@ set -v -e
 kubectl apply -f elasticsearch.deployment.yaml
 kubectl apply -f elasticagent.deployment.yaml
 kubectl apply -f kibana.deployment.yaml
+
+# To start a trial create a Kubernetes secret. Note that it must be in the same namespace as the operator
+# (elastic-system)
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: eck-trial-license
+  namespace: elastic-system
+  labels:
+    license.k8s.elastic.co/type: enterprise_trial
+  annotations:
+    elastic.co/eula: accepted
+EOF
